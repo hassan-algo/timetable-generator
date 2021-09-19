c8923117a7cdfc2023a3b4f03404843d8066e2d1
hassan-algo <hassanalgo12@gmail.com>
2021-09-19 18:42:21 +0500

    added generation system

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ import numpy as np
 import random
 
 
+class GeneticAlgorithem:
+    currentGeneration = []
+
+    def __init__(self, noOfGenerations, generationLength):
+        self.noOfGenerations = noOfGenerations
+        self.generationLength = generationLength
+
+    def generator(self, data):
+        for i in range(self.noOfGenerations):
+            print("Generation No. ", i+1)
+            for j in range(self.generationLength):
+                print("index: ", j+1)
+                self.currentGeneration.append(Person(data))
+
+
+
 class Person:
 
     def __init__(self, data):  # data=[ no of Teacher, no of lectures, no of rooms, no of section ]
@@ -30,7 +46,8 @@ class Person:
         self.data = np.array(self.data)
         print(self.data)
 
-
+    def calculateFittness(self, ):
+        pass
 
 
 if __name__ == "__main__":
@@ -39,10 +56,12 @@ if __name__ == "__main__":
     # Todo: get Class data
     classData = [['A', 'B'], [24, 50]]  # two parameters => 1. Room No. 2. Room Strength
     # Todo: get Student data
-    studentData = [['A', 'B'], [24, 50], ['A', 'B']]  # 3 parameters => 1. Lecture Name 2. Lecture Strength 3. Section name
+    studentData = [['A', 'B'], [24, 50],
+                   ['A', 'B']]  # 3 parameters => 1. Lecture Name 2. Lecture Strength 3. Section name
     # Todo: get Time data
     timetable = [[], []]  # two parameter => 1. Starting Time 2. Ending Time
     # Todo: get lectures data
     lectureData = [['A', 'B'], [1.5, 2]]  # two parameters => 1. Lecture Name 2. Lecture Length
     data = [len(teacherData[0]), len(lectureData[0]), len(classData[0]), len(studentData[2])]
-    person = Person(data)
+    geneticAlgo = GeneticAlgorithem(2, 5)
+    geneticAlgo.generator(data)
